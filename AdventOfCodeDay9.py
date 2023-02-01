d0ff11359ed4b9be1f0d81b85f53880ffd1c563c
DS1123 <68036483+DS1123@users.noreply.github.com>
2023-02-01 08:10:11 +0000

    Add files via upload

---
+++ AdventOfCodeDay9.py
@@ -0,0 +1,44 @@
+import numpy as np
+# Read directions $R , R, U , D , L .. e.t.c ,$ in a list assume both HT start at origin
+directions = ["R", "D", "D", "L" , "L", "L" , "L"] 
+size = len(directions)
+HeadPos = np.array([0,0])
+TailPos = np.array([0,0])
+UniqueListTailPos = [TailPos.tolist()]
+def DrawGrid(TailPos, HeadPos): 
+    for j in range(-size , size +1 ,1  ):
+        for jj in range(-size , size +1 , 1):
+            CurPos = np.array([jj, - j ])
+            if(np.array_equiv(CurPos, TailPos)):
+                if(np.array_equiv(TailPos,HeadPos)):
+                    print("B", end= "" )
+                else:
+                    print("T", end = "")
+            elif(np.array_equiv(CurPos, HeadPos)):
+                print("H", end = "")
+            elif (jj == size  ):
+                print(" \n")
+            else:
+                print("*", end = "")
+    print("\n\n")            
+DrawGrid(TailPos,HeadPos)
+for i in range(size): 
+    if directions[i] == "R" :
+        change = np.array([1,0])
+    if directions[i] == "D" :
+        change = np.array([0,-1])
+    if directions[i] == "U":
+        change = np.array([0,1])
+    if directions[i] == "L":
+        change = np.array([-1,0]) 
+    
+    HeadPos += change 
+    dist2 = (HeadPos[0] - TailPos[0])**2 + (HeadPos[1] - TailPos[1])**2
+    if (dist2 >= 4):
+        # dist^2 \geq 4 means tails must move 
+        TailPos = HeadPos - change
+        if(not(TailPos.tolist() in UniqueListTailPos)):  
+            UniqueListTailPos.append(TailPos.tolist()) 
+    DrawGrid(TailPos,HeadPos)
+print("Number of unique tiles visited by Tails is ", len(UniqueListTailPos)) 
+
